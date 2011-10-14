96c0318cbdba0b4baf2ab4fad1dbc7ae30b5c6e2
nhoening <iam@nicolashoening.de>
2011-10-14 00:04:10 +0000

    for now, we add an admin member if the database has no members so far, so that one can always use that to login to a new setup (id:1, empty passwd)
    git-svn-id: http://subversion.assembla.com/svn/vokomokum/trunk@92 b3c4d6f4-6639-42c7-84c2-d72431877607

--- members/members/models/setup.py
+++ members/members/models/setup.py
@@ -4,6 +4,8 @@ from sqlalchemy.orm import scoped_session
 from sqlalchemy.orm import sessionmaker
 from zope.sqlalchemy import ZopeTransactionExtension
 
+import transaction
+
 DBSession = scoped_session(sessionmaker(extension=ZopeTransactionExtension()))
 Base = declarative_base()
 
@@ -15,6 +17,17 @@ def initialize_sql(engine):
     try:
         session = DBSession()
 
+        # Make sure we have at least our default admin
+        # import late to avoid circular imports (bcs of Base being used
+        # in Member class)
+        from member import Member
+        member1 = session.query(Member).filter(Member.id==1).first()
+        if not member1:
+            member1 = Member(fname='Jim', lname='Segrave')
+            member1.mem_admin = True
+            session.add(member1)
+            session.flush()
+            transaction.commit()
     except IntegrityError:
         DBSession.rollback()
     return session
