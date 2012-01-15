6716ed76a4311eb31822e627292d465a70f777bc
nhoening <iam@nicolashoening.de>
2012-01-15 01:01:52 +0000

    the catch-all error view caused transactions to not be rolled back automatically, setting http status 500 now
    git-svn-id: http://subversion.assembla.com/svn/vokomokum/trunk@126 b3c4d6f4-6639-42c7-84c2-d72431877607

--- members/members/views/base.py
+++ members/members/views/base.py
@@ -33,10 +33,18 @@ class NotFoundView(BaseView):
 
 
 class ErrorView(BaseView):
+    '''
+    A catch-all error view.
+    It will simply show the base template and excuse the eception.
+    The exception text could be shown nicer somehow, maybe we can pass sthg else in addition to msg?
+    To set the status to 500 is important, so that the WSGI stack knows the issue has not actually
+    been resolved, and rolls back any transactions for us.
+    '''
 
     tab = 'home'
 
     def __call__(self):
+        self.request.response.status_int = 500
         return dict(msg="Sorry, something went wrong - maybe you should contact an admin about this. Error message: '%s'" % str(self.context) )
 
 
