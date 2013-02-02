32865f05a06b0c00d0e57320911b6caa9fcc62cd
Nicolas Höning <iam@nicolashoening.de>
2013-02-02 23:43:22 +0100

    fix pyramid version to 1.3, until I know what 1.4 brings

--- members/setup.py
+++ members/setup.py
@@ -7,7 +7,7 @@ README = open(os.path.join(here, 'README.txt')).read()
 CHANGES = open(os.path.join(here, 'CHANGES.txt')).read()
 
 requires = [
-    'pyramid',
+    'pyramid==1.3',
     'SQLAlchemy',
     'transaction',
     'pyramid_tm',
