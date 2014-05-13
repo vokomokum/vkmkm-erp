c4c7ea9134d256b73eb4d20ee6e9e522fb7e2c5d
diederick <diederick.verweij@gmail.com>
2014-05-13 18:14:43 +0200

    added some py-charm meta files..

--- members/scripts/mksqlitedb.py
+++ members/scripts/mksqlitedb.py
@@ -171,7 +171,7 @@ def fillDBRandomly(seed, workgroups):
             ttype = random.choice(ttypes)
             t = Transaction(ttype_id=ttype.id, amount=random.random() * 150,
                             date=datetime.datetime(month[1], month[0],
-                                  random.randint(1, 30)), mem_id=m.mem_id)
+                                  random.randint(1,28)), mem_id=m.mem_id)
             t.ttype = ttype
             t.member = m
             if ttype.pos_neg == 'neg':
