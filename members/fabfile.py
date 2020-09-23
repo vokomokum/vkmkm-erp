9fa44a312813bc8477733f48ddd47c0030d8d563
Nicolas Höning <iam@nicolashoening.de>
2020-09-23 11:09:01 +0200

    upgrade deployment and dev tooling to Python3+ and Fabric2+

--- members/fabfile.py
+++ members/fabfile.py
@@ -1,14 +1,16 @@
-from fabric.api import *
-from fabric.contrib.console import confirm
+import getpass
+
+from fabric import task
+from fabric import Connection, Config
+from invoke import Exit
+from invocations.console import confirm
 
 '''
 This file is very useful in day-to-day Vokomokum development.
 It uses fabric to automate common actions. Install fabric with
 
-$ pip install fabric
+$ pip install "fabric>=2.0" invocations
 
-TODO: This would need to be rewritten to use Fabric >= 2.0 or you install Fabric 1 like this:
-      pip install 'fabric<2.0' (which only works with Python2).
 
 Then it suffices to issue 
 fab develop         # develop the webapp locally
@@ -23,18 +25,26 @@ fab deploy:user=you # deploy latest code on our server and set it 'live'
                     # (you need admin rights for this on the server)
 
 '''
-env.hosts = ['order.vokomokum.nl']
+#vkmkm_hosts = ['voko']
+vkmkm_hosts = ['order.vokomokum.nl']
+path_to_venv = "/home/nicolas/envs/vkmkm"
+venv_activation = "source {}/bin/activate".format(path_to_venv)
 #env.use_ssh_config = True # we may want this
 
-def develop():
+@task
+def develop(c):
     ''' develop the webapp locally '''
-    local('python setup.py develop')
+    with c.prefix(venv_activation):
+        c.run('python setup.py develop')
 
-def serve():
+@task
+def serve(c):
     ''' run local dev server '''
-    local('pserve development.ini --reload')
+    with c.prefix(venv_activation):
+        c.run('pserve development.ini --reload')
 
-def test(standalone=True):
+@task
+def test(c, standalone=True):
     '''
     perfom tests
     '''
@@ -43,56 +53,77 @@ def test(standalone=True):
     # say sthg about an AttributeError. Do this to find out what is wrong:
     # $ python setup.py develop
     # $ python -c "import members.tests.<The module in question>"
-    with settings(warn_only=True):
-        result = local('python setup.py test -q', capture=False)
-    if result.failed and not standalone and not confirm("Tests failed. Continue anyway?"):
-        abort("Aborting at user request.")
+    with c.prefix(venv_activation):
+        result = c.run('python setup.py test -q', hide="stdout", warn=True)
+        if result.failed and not standalone and not confirm("Tests failed. Continue anyway?"):
+            raise Exit("Aborting at user request.")
 
-def commit():
-    ''' commit changed code (interactively)'''
-    local("git add -p && git commit")
+@task
+def commit(c):
+    ''' Commit changed code (interactively)'''
+    c.run("git add -p && git commit")
 
-def push():
-    ''' push commited code to server '''
-    local("git push")
+@task
+def push(c):
+    ''' Push commited code to server '''
+    c.run("git push")
 
-def populate():
-    ''' populate local test DB (sqlite) with dummy data.
+@task
+def populate(c):
+    ''' Populate local test DB (sqlite) with dummy data.
         Uses members-dev.db, which is also named in development.ini as DB
     '''
-    local("python scripts/mksqlitedb.py")
+    with c.prefix(venv_activation):
+        c.run("python scripts/mksqlitedb.py")
 
-def prepare_deploy():
+@task
+def prepare_deploy(c):
     '''
-    run tests and if successful, push all code to git
+    Run tests and if successful, push all code to git
     (actually runs fab test, fab commit and fab push)
     '''
-    test(standalone=False)
-    commit()
-    push()
+    test(c, standalone=False)
+    commit(c)
+    push(c)
 
-def deploy(user='you', mode="test", branch="master"):
+@task(hosts=vkmkm_hosts)
+def deploy(c, user=None, mode="test", branch="master"):
     '''
-    deploy latest code on our server and set it 'live'
+    Deploy latest code on our server and set it 'live'
     (you need admin rights for this on the server)
-    Be sure to use :user=you for sudo commands to succeed.
-    Use :user=you,mode=production to update the production app.
+    Be sure to use --user=you for sudo commands to succeed.
+    Also, use --mode=production to update the production app.
+    Finally, fabric accepts a few parameters so you handle your ssh key
+    correctly. For instance, you might need to use:
+    --identity=path/to/your/provate/key
+    --prompt-for-passphrase
+    See Fabrics's (>=2.0) documentation for more details.
     '''
-    test(standalone=False)
-    push()
-    env.user = user
-    code_dir = '/var/voko/git-repo'
-    # this is only useful when we'd run multiple server nodes
-    with settings(warn_only=True):
-        if run("test -d {}".format(code_dir)).failed:
-            sudo("git clone git@github.com:vokomokum/vkmkm-erp.git {}".\
-                     format(code_dir), user=user)
-    with cd(code_dir):
-        sudo("./dev-tools/update-members-site-from-git {} {}".format(mode, branch))
-        # no need to restart apache, simply touch wsgi file (when in daemon mode)
-        # (see http://code.google.com/p/modwsgi/wiki/ReloadingSourceCode)
-        if mode == 'production':
-            sudo("touch /var/www/members/pyramid.wsgi", user=user)
-        else:
-            sudo("touch /var/www/memberstest/pyramid.wsgi", user=user)
-        #run(sudo("/etc/init.d/apache2 restart"))
+    if not user:
+        raise Exit("Please provide a user name for use on the server")
+
+    # make sure we don't deploy untested or outdated code
+    #test(c, standalone=False)  # Here, c is a Connection, not a Context, so this would need work
+    #push(c)
+    if not confirm("have you tested and pushed your code??"):
+        raise Exit("Aborting at user request.")
+    
+    code_dir = "/var/voko/git-repo"
+    app_dir = "/var/www"
+    # make sure we have the code checked out    
+    if not c.run(f"test -d {code_dir}", warn=True):
+        c.run(f"git clone git@github.com:vokomokum/vkmkm-erp.git {code_dir}", user=user)
+    
+    # make sure sudo command can be used
+    sudo_pass = getpass.getpass(f"What's the sudo password for user {user}?")
+    c.config = Config(overrides={'sudo': {'password': sudo_pass}})
+    
+    #with cd(code_dir):  # not yet implemented in fabric2 and sudo does not remember cd
+    c.sudo(f"{code_dir}/dev-tools/update-members-site-from-git {mode} {branch}")
+    # no need to restart apache, simply touch wsgi file (when in daemon mode)
+    # (see http://code.google.com/p/modwsgi/wiki/ReloadingSourceCode)
+    if mode == 'production':
+        c.sudo(f"touch {app_dir}/members/pyramid.wsgi")
+    else:
+        c.sudo(f"touch {app_dir}/memberstest/pyramid.wsgi")
+    #c.sudo("/etc/init.d/apache2 restart")
